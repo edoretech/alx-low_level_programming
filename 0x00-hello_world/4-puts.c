1bc12be857725cf8b47234634f3c43f3e3339885
edoretech <edoreprecious@gmail.com>
2023-07-13 20:27:42 -0700

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,3 @@
 #include <stdio.h>
 /**
-* main - A program that prints a line with puts function
-* Return:0(success)
-*/
-init main(void)
-{
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-}
+ * main - 
