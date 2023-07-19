bd8bd199811325537d9831f29f78d67531441659
edoretech <edoreprecious@gmail.com>
2023-07-19 08:11:41 -0700

    ./2-alphabet_x10

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - Make the alphabet
+ *
+ * Return:void
+ */
+
+int print_alphabet(void)
+ {
+char ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}  
