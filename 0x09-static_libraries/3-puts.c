5074e2b333d5bbdf28dca19ed6ab13f85036d813
edoretech <edoreprecious42@gmail.com>
2023-08-22 07:27:58 -0700

    grace is cool

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: string to print
+ * Return:void
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		putchar(*str++);
+	}
+		putchar('\n');
+}
